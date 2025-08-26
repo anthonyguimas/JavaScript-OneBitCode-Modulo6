933d73947d21d8a33714faadacc0cc7961cad4ae
anthonyguimas <anthonygabriel028@gmail.com>
2025-08-25 21:11:46 -0300

    Funções async e utilizando o async

---
+++ FuncoesAsync/index.js
@@ -0,0 +1,30 @@
+function asyncSum(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("arguments must be of type number");
+    } else {
+      resolve(a + b);
+    }
+  });
+}
+
+function asyncSubstract(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("arguments must be of type number");
+    } else {
+      resolve(a - b);
+    }
+  });
+}
+
+const sumResult = asyncSum(50, 33);
+const subtractResult = asyncSubstract(50, 33);
+
+Promise.all([sumResult, subtractResult])
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
